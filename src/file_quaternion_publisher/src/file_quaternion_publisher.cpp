72ba21d5bb80019c4c75ed05de30a26a01c2d026
ElenaCurti <elena.curti.00@gmail.com>
2024-02-03 04:07:25 -0800

    Presi RPY. Sistemati commenti e indentazione. Messo pkill rviz2. Cambiamo file configurazione rviz

--- src/file_quaternion_publisher/src/file_quaternion_publisher.cpp
+++ src/file_quaternion_publisher/src/file_quaternion_publisher.cpp
@@ -36,91 +36,93 @@ int main(int argc, char *argv[])
   // Open the file
   std::ifstream file(file_path);
 
-    // Check if the file is opened successfully
-    if (!file.is_open()) {
-        RCLCPP_ERROR(node->get_logger(), "Error opening file: %s", file_path.c_str());
-        return 1;
-    }
+  // Check if the file is opened successfully
+  if (!file.is_open()) {
+      RCLCPP_ERROR(node->get_logger(), "Error opening file: %s", file_path.c_str());
+      return 1;
+  }
 
-    auto quaternion_pub = node->create_publisher<nav_msgs::msg::Odometry>("odom/prova", 10);
-    auto quaternion_pub_original = node->create_publisher<nav_msgs::msg::Odometry>("odom/original", 10);
+  // Due nodi per pubblicare il quaternione originale di orbslam (in verde) e quello da noi ruotato (in rosso)
+  auto quaternion_pub_original = node->create_publisher<nav_msgs::msg::Odometry>("odom/original", 10);
+  auto quaternion_pub = node->create_publisher<nav_msgs::msg::Odometry>("odom/prova", 10);
 
-    // Read the file line by line
-    std::cout << "Apri rviz2 e sottoscriviti a odom/prova. Il frame_id e' map. Poi premi un tasto qui per continuare..." << std::endl;
-    system("rviz2 -d visualizza_odom.rviz  >/dev/null 2>/dev/null  & ");
-    rclcpp::sleep_for(std::chrono::milliseconds(5*1000));
+  system("rviz2 -d visualizza_odom.rviz  >/dev/null 2>/dev/null  & ");
+  rclcpp::sleep_for(std::chrono::milliseconds(5*1000));
 
-    std::string line;
-    //std::cin.get();
+  std::string line;
+  // Read the file line by line
+  while (std::getline(file, line)) {
+    // Use std::istringstream to separate values
+    std::istringstream iss(line);
+    float togli_primo_valore;
+  
+    // Ricaviamo e pubblichiamo il quaternione originale di orbslam
+    Eigen::Quaternionf q;
+    Eigen::Vector3f twc;
+    std::istringstream ss(line);
+    ss >> togli_primo_valore>> twc(0) >> twc(1) >> twc(2) >> q.x() >> q.y() >> q.z() >> q.w() ;
 
-    while (std::getline(file, line)) {
-        // Use std::istringstream to separate values
-        std::istringstream iss(line);
-      
-        auto message = nav_msgs::msg::Odometry();
-        message.header.frame_id = "map";
+    auto message = nav_msgs::msg::Odometry();
+    message.header.frame_id = "map";        
 
+    geometry_msgs::msg::Pose output_pose{};
 
-        float togli_primo_valore;
+    output_pose.position.x = twc(0);
+    output_pose.position.y = twc(2);
+    output_pose.position.z = 0 ;
 
-        Eigen::Quaternionf q;
-        Eigen::Vector3f twc;
-        std::istringstream ss(line);
-        ss >> togli_primo_valore>> twc(0) >> twc(1) >> twc(2) >> q.x() >> q.y() >> q.z() >> q.w() ;
+    output_pose.orientation.x = q.x();
+    output_pose.orientation.y = q.y();
+    output_pose.orientation.z = q.z();
+    output_pose.orientation.w = q.w();
 
-        geometry_msgs::msg::Pose output_pose{};
-
-        output_pose.position.x = twc(0);
-        output_pose.position.y = twc(2);
-        output_pose.position.z = 0 ;
-
-        output_pose.orientation.x = q.x();
-        output_pose.orientation.y = q.y();
-        output_pose.orientation.z = q.z();
-        output_pose.orientation.w = q.w();
-
-        message.pose.pose = output_pose;
-        
-        quaternion_pub_original->publish(message);
+    message.pose.pose = output_pose;    
+    quaternion_pub_original->publish(message);
 
 
-        // Nuovo messaggio con quat ruotato
-        auto message2 = nav_msgs::msg::Odometry();
-        message2.header.frame_id = "map";
+    // Nuovo messaggio: ci copiamo la position
+    auto message2 = nav_msgs::msg::Odometry();
+    message2.header.frame_id = "map";
 
-        geometry_msgs::msg::Pose output_pose_ruotato{}; 
-        output_pose_ruotato.position.x = twc(0);
-        output_pose_ruotato.position.y = twc(2);
-        output_pose_ruotato.position.z = 0 ;
+    geometry_msgs::msg::Pose output_pose_ruotato{}; 
+    output_pose_ruotato.position.x = twc(0);
+    output_pose_ruotato.position.y = twc(2);
+    output_pose_ruotato.position.z = 0 ;
 
-        // Ruotiamo il quaternion
-        
-        tf2::Quaternion tf2_quat;
-        tf2::fromMsg(output_pose.orientation, tf2_quat);
-        std::cout   << "\t" << output_pose.orientation.x << " " <<  output_pose.orientation.y << " " <<  output_pose.orientation.z  << " " <<  output_pose.orientation.w  << std::endl;
-        //std::cout   << "\t" << tf2_quat_from_msg.getX() << " " <<  tf2_quat_from_msg.getY() << " " <<  tf2_quat_from_msg.getZ()  << " " <<  tf2_quat_from_msg.getW()  << std::endl;
-        tf2_quat.setRPY( 0.0, 0.0, degreesToRadians(90));
-        output_pose_ruotato.orientation = tf2::toMsg(tf2_quat);
+    
+    // Copiamo il quaternione
+    tf2::Quaternion tf2_quat;
+    tf2::fromMsg(output_pose.orientation, tf2_quat);
+    //std::cout   << "\t" << output_pose.orientation.x << " " <<  output_pose.orientation.y << " " <<  output_pose.orientation.z  << " " <<  output_pose.orientation.w  << std::endl;
+    //std::cout   << "\t" << tf2_quat_from_msg.getX() << " " <<  tf2_quat_from_msg.getY() << " " <<  tf2_quat_from_msg.getZ()  << " " <<  tf2_quat_from_msg.getW()  << std::endl;
+    
+    // Prendiamo roll, pitch e yaw del quaternione originale
+    tf2::Matrix3x3 m(tf2_quat);
+    double roll, pitch, yaw;
+    m.getRPY(roll, pitch, yaw);
+    std::cout   << "\t" << roll << " " << pitch << " " << yaw << std::endl;
 
-        message2.pose.pose = output_pose_ruotato;
+    // Ruotiamo e pubblichiamo il nuovo quaternione
+    tf2_quat.setRPY( 0.0, 0.0, yaw+degreesToRadians(90));
 
-        quaternion_pub->publish(message2);
+    output_pose_ruotato.orientation = tf2::toMsg(tf2_quat);
+    message2.pose.pose = output_pose_ruotato;
+    quaternion_pub->publish(message2);
+
+    // Sleep for a short duration (adjust as needed)
+    rclcpp::sleep_for(std::chrono::milliseconds(10));
+  }
+
+  // Close the file
+  file.close();
 
 
+  std::string tmp;
+  std::cout << "Inserisci qualcosa per terminare..."; 
+  std::cin >>  tmp;
+  //std::cin.get();
 
-        
-
-        
-
-
-        
-
-        // Sleep for a short duration (adjust as needed)
-        rclcpp::sleep_for(std::chrono::milliseconds(10));
-    }
-
-    // Close the file
-    file.close();
+  system("pkill rviz2");
 
 
   rclcpp::shutdown();
